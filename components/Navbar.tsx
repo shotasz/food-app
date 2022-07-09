82585213ac0294adfee267a526efa3c65765702c
shotasz <s2shota123@gmail.com>
2022-07-09 14:23:14 +0900

    navbarコンポーネントにauthAPIをコミット

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,13 +2,20 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { GoogleLogin, GoogleLogout } from "react-google-login";
+
 import { AiOutlineLogout } from "react-icons/ai";
+import { GoogleLogin, googleLogout } from "@react-oauth/google";
 import { BiSearch } from "react-icons/bi";
 import { IoMdAdd } from "react-icons/io";
+
 import Logo from "../utils/food-app-logo.png";
+import { createOrGetUser } from "../utils";
+
+import useAuthStore from "../store/authStore";
 
 const Navbar = () => {
+  const { userProfile, addUser, removeUser } = useAuthStore();
+
   return (
     <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4 bg-secondary">
       <Link href="/">
@@ -22,6 +29,50 @@ const Navbar = () => {
           />
         </div>
       </Link>
+
+      <div>SEARCH</div>
+
+      <div>
+        {userProfile ? (
+          <div className="flex gap-5 md:gap-10">
+            <Link href="/upload">
+              <button className="border-2 px-2 md:px-4 text-base font-semibold flex items-center gap-2">
+                <IoMdAdd className="text-xl" /> {` `}
+                <span className="hidden md:block">Upload</span>
+              </button>
+            </Link>
+            {userProfile.image && (
+              <Link href="/">
+                <>
+                  <Image
+                    width={40}
+                    height={40}
+                    className="rounded-full cursor-pointer"
+                    src={userProfile.image}
+                    alt="profile photo"
+                    priority={true}
+                  />
+                </>
+              </Link>
+            )}
+            <button
+              type="button"
+              className="px-2"
+              onClick={() => {
+                googleLogout();
+                removeUser();
+              }}
+            >
+              <AiOutlineLogout color="red" fontSize={21} />
+            </button>
+          </div>
+        ) : (
+          <GoogleLogin
+            onSuccess={(response) => createOrGetUser(response, addUser)}
+            onError={() => console.log("Error")}
+          />
+        )}
+      </div>
     </div>
   );
 };
