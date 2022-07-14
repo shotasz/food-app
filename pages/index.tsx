e68e11de2701b7f4282d4254602d58cd57971bdc
shotasz <s2shota123@gmail.com>
2022-07-14 20:39:48 +0900

    環境設定を追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,8 @@ import NoResults from "../components/NoResults";
 import VideoCard from "../components/VideoCard";
 import { Video } from "../types";
 
+import { BASE_URL } from "../utils";
+
 interface IProps {
   videos: Video[];
 }
@@ -21,7 +23,7 @@ const Home = ({ videos }: IProps) => {
 };
 
 export const getServerSideProps = async () => {
-  const { data } = await axios.get(`http://localhost:3000/api/post`);
+  const { data } = await axios.get(`${BASE_URL}/api/post`);
 
   return {
     props: {
