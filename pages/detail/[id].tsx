492c2a5046998b1af5a1fe0c58280459cde13f1e
shotasz <s2shota123@gmail.com>
2022-07-18 10:53:26 +0900

    detailページコミット

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -25,6 +25,8 @@ const Detail = ({ postDetails }: IProps) => {
   const videoRef = useRef<HTMLVideoElement>(null);
   const router = useRouter();
   const { userProfile }: any = useAuthStore();
+  const [comment, setComment] = useState("");
+  const [isPostingComment, setIsPostingComment] = useState(false);
 
   const onVideoClick = () => {
     if (playing) {
@@ -51,6 +53,23 @@ const Detail = ({ postDetails }: IProps) => {
       });
 
       setPost({ ...post, likes: data.likes });
+    }
+  };
+
+  const addComment = async (e: any) => {
+    e.preventDefault();
+
+    if (userProfile && comment) {
+      setIsPostingComment(true);
+
+      const { data } = await axios.put(`${BASE_URL}/api/post/${post._id}`, {
+        userId: userProfile._id,
+        comment,
+      });
+
+      setPost({ ...post, comments: data.comments });
+      setComment("");
+      setIsPostingComment(false);
     }
   };
 
@@ -142,7 +161,13 @@ const Detail = ({ postDetails }: IProps) => {
               )}
             </div>
 
-            <Comments />
+            <Comments
+              comment={comment}
+              setComment={setComment}
+              addComment={addComment}
+              comments={post.comments}
+              isPostingComment={isPostingComment}
+            />
           </div>
         </div>
       </div>
