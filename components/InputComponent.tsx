e12cc03d7fc33d38ae1501e1cf7161db185939ff
shotasz <s2shota123@gmail.com>
2022-08-07 10:08:32 +0900

    InputComponentをコミット

--- components/InputComponent.tsx
+++ components/InputComponent.tsx
@@ -1,10 +1,27 @@
-import React, { ChangeEvent } from "react";
+import React, { ChangeEvent, Dispatch, SetStateAction, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import useAuthStore from "../store/authStore";
 
 interface IProps {
   ingredient: string;
   servings: string;
-  handleChangeInput: (id: string, event: ChangeEvent<HTMLInputElement>) => void;
+  handleChangeInput: (
+    id: string,
+    event: ChangeEvent<HTMLInputElement>,
+    state: IIngredients[],
+    setState: Dispatch<SetStateAction<IIngredients[]>>
+  ) => void;
   id: string;
+  inputFields: IIngredients[];
+  setInputFields: Dispatch<SetStateAction<IIngredients[]>>;
+}
+
+interface IIngredients {
+  _type: string;
+  _key: string;
+  id: string;
+  servings: string;
+  ingredient: string;
 }
 
 const InputComponent = ({
@@ -12,29 +29,80 @@ const InputComponent = ({
   ingredient,
   handleChangeInput,
   id,
+  inputFields,
+  setInputFields,
 }: IProps) => {
+  const { userProfile }: { userProfile: any } = useAuthStore();
+  const handleAddFields = () => {
+    setInputFields([
+      ...inputFields,
+      {
+        _type: "ingredients",
+        _key: userProfile?._id,
+        id: uuidv4(),
+        servings: "",
+        ingredient: "",
+      },
+    ]);
+  };
+
+  const handleRemoveFields = (id: string) => {
+    const values = [...inputFields];
+    values.splice(
+      values.findIndex((value) => value.id === id),
+      1
+    );
+    setInputFields(values);
+  };
   return (
     <div>
       <div className="flex">
-        <div>
+        <div className="mr-2">
           <label className="text-sm font-medium">材料</label>
           <input
             type="text"
             name="ingredient"
             value={ingredient}
-            onChange={(event) => handleChangeInput(id, event)}
+            onChange={(event) =>
+              handleChangeInput(id, event, inputFields, setInputFields)
+            }
             className="rounded outline-none text-base border-2 border-gray-200 p-2 w-full"
           />
         </div>
-        <div>
+        <div className="mr-2">
           <label className="text-sm font-medium">単位</label>
           <input
             type="text"
             name="servings"
             value={servings}
-            onChange={(event) => handleChangeInput(id, event)}
+            onChange={(event) =>
+              handleChangeInput(id, event, inputFields, setInputFields)
+            }
             className="rounded outline-none text-base border-2 border-gray-200 p-2 w-full"
           />
+        </div>
+
+        {inputFields.length > 1 ? (
+          <div className="self-end mr-2">
+            <button
+              onClick={() => handleRemoveFields(id)}
+              type="button"
+              className="h-[44px] w-16 p-2 bg-red-500 text-white text-base rounded"
+            >
+              削除
+            </button>
+          </div>
+        ) : (
+          ""
+        )}
+        <div className="self-end">
+          <button
+            onClick={handleAddFields}
+            type="button"
+            className="h-[44px] w-16 p-2 bg-[#74CC2D] text-white text-base rounded"
+          >
+            追加
+          </button>
         </div>
       </div>
     </div>
