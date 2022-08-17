50c8e74c7c1fc392de4ba8fd61be6d009d58225a
shotasz <s2shota123@gmail.com>
2022-08-17 11:47:19 +0900

    uploadpatchの投稿レシピをコミット

--- pages/upload/[id].tsx
+++ pages/upload/[id].tsx
@@ -1,13 +1,14 @@
 import { SanityAssetDocument } from "@sanity/client";
 import Image from "next/image";
 import axios from "axios";
-import React, { ChangeEvent, useState } from "react";
+import React, { ChangeEvent, useEffect, useState } from "react";
 import { FaCloudUploadAlt } from "react-icons/fa";
 import useAuthStore from "../../store/authStore";
 import { topics } from "../../utils/constants";
 import { Video } from "../../types";
 import { BASE_URL } from "../../utils";
 import { v4 as uuidv4 } from "uuid";
+import { client } from "../../utils/client";
 import InputComponent from "../../components/InputComponent";
 import InputRecipes from "../../components/InputRecipes";
 import { useRouter } from "next/router";
@@ -17,28 +18,70 @@ interface IProps {
 }
 
 const UploadPatch = ({ data }: IProps) => {
-  const { caption, video, ingredients, recipes } = data;
+  const { caption, video, ingredients, recipes, topic } = data;
   const [isLoading, setIsLoading] = useState(false);
   const [wrongFileType, setWrongFileType] = useState(false);
   const router = useRouter();
   const { userProfile }: { userProfile: any } = useAuthStore();
-  const [inputFields, setInputFields] = useState([
-    {
-      _type: "ingredients",
-      _key: userProfile?._id,
-      id: uuidv4(),
-      servings: "",
-      ingredient: "",
-    },
-  ]);
-  const [inputRecipes, setInputRecipes] = useState([
-    {
-      _type: "recipes",
-      _key: userProfile?._id,
-      id: uuidv4(),
-      recipe: "",
-    },
-  ]);
+
+  const [imageAsset, setImageAsset] = useState<
+    SanityAssetDocument | undefined
+  >();
+  const [changeCaption, setChangeCaption] = useState(caption);
+  const [changeCategory, setChangeCategory] = useState(topic);
+  const [inputFields, setInputFields] = useState(ingredients);
+  const [inputRecipes, setInputRecipes] = useState(recipes);
+
+  const uploadRecipe = async (e: any) => {
+    const selectedFile = e.target.files[0];
+    const fileTypes = ["image/jpeg", "image/png"];
+
+    if (fileTypes.includes(selectedFile.type)) {
+      setIsLoading(true);
+      client.assets
+        .upload("file", selectedFile, {
+          contentType: selectedFile.type,
+          filename: selectedFile.name,
+        })
+        .then((data) => {
+          setImageAsset(data);
+          setIsLoading(false);
+        });
+    } else {
+      setIsLoading(false);
+      setWrongFileType(true);
+    }
+  };
+
+  //   const handlePost = async () => {
+  //     if (caption && imageAsset?._id && category && inputFields && recipes) {
+  //       setSavingPost(true);
+
+  //       const document = {
+  //         _type: "post",
+  //         caption,
+  //         video: {
+  //           _type: "file",
+  //           asset: {
+  //             _type: "reference",
+  //             _ref: imageAsset?._id,
+  //           },
+  //         },
+  //         userId: userProfile?._id,
+  //         postedBy: {
+  //           _type: "postedBy",
+  //           _ref: userProfile?._id,
+  //         },
+  //         topic: category,
+  //         ingredients: inputFields,
+  //         recipes: recipes,
+  //       };
+
+  //       await axios.post(`${BASE_URL}/api/post`, document);
+
+  //       router.push("/");
+  //     }
+  //   };
 
   const handleChangeInput = (
     id: string,
@@ -66,50 +109,49 @@ const UploadPatch = ({ data }: IProps) => {
               レシピのイメージ画像をここで投稿して下さい
             </p>
           </div>
-          <div className="border-dashed rounded-xl md:border-4 border-2 border-gray-200 flex flex-col justify-center items-center outline-none mt-10 md:w-[360px] w-[320px] md:h-[460px] h-[420px] p-10 cursor-pointer hover:border-red-300 hover:bg-gray-100">
+          <div className="border-dashed rounded-xl md:border-4 border-2 border-gray-200 flex flex-col justify-center items-center outline-none mt-10 md:w-[400px] w-[320px] md:h-[600px] h-[520px] p-10 hover:border-red-300 hover:bg-gray-100">
             {isLoading ? (
               <p>アップロード中...</p>
             ) : (
               <div>
-                {video ? (
-                  <div>
-                    <Image
-                      width={300}
-                      height={400}
-                      className="h-full w-full object-cover"
-                      src={video.asset.url}
-                      alt="profile photo"
-                      priority={true}
-                    />
-                  </div>
-                ) : (
-                  <label className="cursor-pointer">
-                    <div className="flex flex-col items-center justify-center h-full">
-                      <div className="flex flex-col items-center justify-center">
-                        <p className="font-bold text-xl">
-                          <FaCloudUploadAlt className="text-gray-300 text-6xl" />
-                        </p>
-                        <p className="text-xl font-semibold">
-                          画像をアップロード
-                        </p>
+                <label>
+                  <div className="flex flex-col items-center justify-center h-full">
+                    {imageAsset ? (
+                      <div>
+                        <Image
+                          width={300}
+                          height={400}
+                          className="h-full w-full object-cover"
+                          src={imageAsset.url}
+                          alt="profile photo"
+                          priority={true}
+                        />
                       </div>
-                      <p className="text-gray-400 text-center mt-10 text-sm leading-10">
-                        pngまたはjpg <br />
-                        640x960以上 <br />
-                        5MB以内のファイルを選択して下さい <br />
-                      </p>
-                      <p className="bg-[#74CC2D] text-center mt-10 rounded text-white text-base font-medium p-2 w-52 outline-none">
-                        ファイルを選択
-                      </p>
-                    </div>
+                    ) : (
+                      <div>
+                        <Image
+                          width={300}
+                          height={400}
+                          className="h-full w-full object-cover"
+                          src={video.asset.url}
+                          alt="profile photo"
+                          priority={true}
+                        />
+                      </div>
+                    )}
+
+                    <p className="bg-[#74CC2D] text-center mt-10 rounded text-white text-base font-medium p-2 md:w-1/2 w-full outline-none cursor-pointer">
+                      ファイルを選択
+                    </p>
+
                     <input
                       type="file"
                       name="upload-recipe"
-                      onChange={() => {}}
+                      onChange={uploadRecipe}
                       className="w-0 h-0"
                     />
-                  </label>
-                )}
+                  </div>
+                </label>
               </div>
             )}
             {wrongFileType && (
@@ -119,33 +161,33 @@ const UploadPatch = ({ data }: IProps) => {
             )}
           </div>
         </div>
-        <div className="flex flex-col gap-3 mt-20 md:w-[360px] w-[320px]">
+        <div className="flex flex-col gap-3 md:mt-20 mt-0 md:w-[360px] w-[320px]">
           <label className="text-base font-medium">レシピ名</label>
           <input
             type="text"
-            value={caption}
-            onChange={() => {}}
+            value={changeCaption}
+            onChange={(e) => setChangeCaption(e.target.value)}
             className="rounded outline-none text-base border-2 border-gray-200 p-2"
           />
 
           <label className="text-base font-medium">カテゴリー選択</label>
           <select
-            onChange={() => {}}
+            onChange={(e) => setChangeCategory(e.target.value)}
             className="outline-none text-base border-2 border-gray-200 p-2 capitalize lg:p-4 rounded cursor-pointer"
           >
-            {topics.map((topic) => (
+            {topics.map((item) => (
               <option
-                key={topic.name}
+                key={item.name}
                 className="outline-none capitalize bg-white text-gray-700 text-base p-2 hover:bg-slate-300"
-                value={topic.name}
+                value={item.name}
               >
-                {topic.ja}
+                {item.ja}
               </option>
             ))}
           </select>
 
           <label className="text-base font-medium">レシピ</label>
-          {ingredients.map((ingredient, idx) => (
+          {inputFields.map((ingredient, idx) => (
             <div key={ingredient.id}>
               <InputComponent
                 id={ingredient.id}
@@ -160,7 +202,7 @@ const UploadPatch = ({ data }: IProps) => {
           ))}
 
           <label className="text-base font-medium">作り方</label>
-          {recipes.map((recipe, idx) => (
+          {inputRecipes.map((recipe, idx) => (
             <div key={recipe.id}>
               <InputRecipes
                 id={recipe.id}
